629e0baa78ec06cf18f578b3d827f6171f93c0c7
Hieu Ba <phambah08@gmail.com>
2020-05-13 12:09:58 +0700

    code components

--- src/components/DCandidates.js
+++ src/components/DCandidates.js
@@ -1,7 +1,82 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from "react-redux";
+import * as action from "../actions/dCandiate"
+import { Grid, Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, withStyles, ButtonGroup, Button } from "@material-ui/core";
+import DCandidateForm from "./DCandidateForm";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { useToasts } from "react-toast-notifications";
 
-const DCandidates = ({classes, ...props})=>{
+const styles = theme => ({
+   root: {
+      "& .MuiTableCell-head": {
+         fontSize: "1.24rem"
+      }
+   },
+   pager: {
+      margion: theme.spacing(2),
+      padding: theme
+   }
+})
 
-    const []
+const DCandidates = ({ classes, ...props }) => {
+
+   const [currentId, setCurrentId] = useState(0);
+   const onDelete = id => {
+      if (window.confirm('Are you want to delete')) {
+         props.deleteDCandidate(id, () => addToast('Delete successfully', { appearance: 'info' }))
+      }
+   }
+
+   return (
+      <Pager className={classes.pager} elevation={3}>
+         <Grip container>
+            <Grid item xs={6}>
+               <DCandidateForm {...({ currentId, setCurrentId })} />
+            </Grid>
+            <Grid item xs={6}>
+               <Table>
+                  <TableHead>
+                     <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Mobile</TableCell>
+                        <TableCell>Blood group</TableCell>
+                        <TableCell></TableCell>
+                     </TableRow>
+                  </TableHead>
+                  <TableBody>
+                     {
+                        props.dCandiateList.map((record, index) => {
+                           return (<TableRow key={index} hover>
+                              <TableCell>{record.fullName}</TableCell>
+                              <TableCell>{record.mobile}</TableCell>
+                              <TableCell>{record.bloodGroup}</TableCell>
+                              <TableCell><ButtonGroup variant="text">
+                                 <Button><EditIcon color="primary"
+                                    onClick={() => { setCurrentId(record.id) }} /></Button>
+                                 <Button><DeleteIcon color="secondary"
+                                    onClick={() => onDelete(record.id)} /></Button>
+                              </ButtonGroup></TableCell>
+
+                           </TableRow>)
+                        })
+                     }
+                  </TableBody>
+               </Table>
+            </Grid>
+         </Grip>
+      </Pager>
+   )
 
 }
+
+const mapStateToProps = state => ({
+   dCandiateList: state.dCandiate.list
+})
+
+const mapActionToProps = {
+   fetchAllDCandidates: action.fetchAll,
+   deleteDCandidate: action.Delete
+}
+
+export default connect(mapStateToProps, mapActionToProps)(withStyles(styles)(DCandidates));
